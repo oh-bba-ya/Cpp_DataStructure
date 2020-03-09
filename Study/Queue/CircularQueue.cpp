1b69d14df9ff00f071b272e36655995fac497850
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-03-09 20:57:42 +0900

    Create CircularQueue.cpp

---
+++ Study/Queue/CircularQueue.cpp
@@ -0,0 +1,16 @@
+#include "CircularQueue.h"
+
+
+
+
+void main() {
+	CircularQueue que;
+	for (int i = 1; i < 10; i++) {
+		que.enqueue(i);
+	}
+	que.display();
+	que.dequeue();
+	que.dequeue();
+	que.dequeue();
+	que.display();
+}
