0d4085f16d2db678ded7c64233eef0e553c50d18
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-03-10 04:16:15 +0900

    Update Student.h

--- Study/Stack_03/Student.h
+++ Study/Stack_03/Student.h
@@ -19,8 +19,9 @@ public:
 };
 
 /*
+이 오류는 Chapter3에서는 문제가 없지만 Chapter05 Node에서 문제가 생긴다 또 적어 놓겠다.
 14라인을 확인 하면 오류가 난다고 했었는데 char* n = ""   -> char* n =(char*) "" 로 바꾸게 되면은 해결이 된다. 
 이유는  char *에 문자열 리터럴을 담는 방식은 C 스타일이며, C++에서는 잘못된 코딩방법이기 때문에 라고나와있다.
-{출처:http://egloos.zum.com/kim0522ms/v/6438724dl
+{출처:http://egloos.zum.com/kim0522ms/v/6438724dl}
 */
 
