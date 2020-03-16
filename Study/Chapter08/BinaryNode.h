47eaa315ccf04fdc4424f5e9dea99ca813124a1f
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-03-17 00:42:50 +0900

    Update BinaryNode.h

--- Study/Chapter08/BinaryNode.h
+++ Study/Chapter08/BinaryNode.h
@@ -5,16 +5,16 @@ using namespace std;
 
 class BinaryNode {
 protected:
-	int data;				//트리에 저장할 데이터.
+	char data;				//트리에 저장할 데이터.
 	BinaryNode* left;		// 왼쪽 자식 노드의 포인터.
 	BinaryNode* right;		// 오른쪽 자식 노드의 포인터.
 public:
-	BinaryNode(int val=0, BinaryNode* l= NULL, BinaryNode* r= NULL):
+	BinaryNode(char val=0, BinaryNode* l= NULL, BinaryNode* r= NULL):
 		data(val),left(l),right(r){}
-	void setData(int val) { data = val; }
+	void setData(char val) { data = val; }
 	void setLeft(BinaryNode* l) { left = l; }
 	void setRight(BinaryNode* r) { right = r; }
-	int getData() { return data; }
+	char getData() { return data; }
 	BinaryNode* getLeft() { return left; }
 	BinaryNode* getRight() { return right; }
 	bool isLeaf() { return left == NULL && right == NULL; }
