c33922bffa8d20b67f15ecd54830598e8c55b19f
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2020-03-26 00:23:46 +0900

    Update 01.cpp

--- Problem/Chapter01/01.cpp
+++ Problem/Chapter01/01.cpp
@@ -1,1 +1,25 @@
-sdaf
+//문제 1
+// sum = n(n+1)/2 공식을 사용하여 int sumAlgorithmA를 구현하라.
+
+#include <iostream>
+using namespace std;
+
+int sumAlgorithmA(int n);
+
+void main() {
+	int num;
+	cout << "숫자를 입력해주세요: ";
+	cin >> num;
+
+	cout << "결과 : ";
+	cout << sumAlgorithmA(10) << endl;
+
+
+
+}
+
+int sumAlgorithmA(int n) {
+	int result;
+	result = n*(n + 1) / 2;
+	return result;
+}
